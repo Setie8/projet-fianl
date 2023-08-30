f882e68db1a7da91468c1cf054ba834adf9105d7
Jeremy Niondo <jeremieniondo@gmail.com>
2023-08-30 13:15:37 +0000

    responsive show

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,20 @@
 var header = document.getElementsByTagName("header")[0];
-console.log(header)
+var navbarLeft = document.querySelector('.navbar-left');
+var navbarRight = document.querySelector('.navbar-right');
+var menuIcon = document.querySelector('#menu-icon');
+
 window.addEventListener("scroll", ()=>{
     window.scrollY > 0 ? header.classList.add("headerBg") : header.classList.remove("headerBg")
+})
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbarLeft.classList.toggle('active');
+    navbarRight.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+    menuIcon.classList.remove('bx-x');
+    navbarLeft.classList.remove('active');
+    navbarRight.classList.remove('active');
 })
